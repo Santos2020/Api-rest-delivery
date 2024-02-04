8b9b2a831f5bb992e9e336ca79464f0f4e0e562c
carlos santos <carlos.aes.1977@gmail.com>
2024-02-04 19:34:59 -0300

    corecao class na var preticates

--- src/main/java/com/restaurante/restfood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/restaurante/restfood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -3,7 +3,6 @@ package com.restaurante.restfood.infrastructure.repository;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -25,38 +24,35 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 	private EntityManager manager;
 
 	@Override
-	public List<Restaurante> find(String nome,
-			BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 		Root<Restaurante> root = criteria.from(Restaurante.class);
-		
+
 		var predicates = new ArrayList<Predicate>();
-		
-		if(StringUtils.hasLength(nome)) {
-		predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
-		
+
+		if (StringUtils.hasText(nome)) {
+			predicates.add(builder.like(root.get("nome"), "%" + nome + "%"));
+
 		}
-		
-		if(taxaFreteInicial != null) {
-		predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
-		
+
+		if (taxaFreteInicial != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial));
+
 		}
-		
-		if(taxaFreteFinal != null) {
-		predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
+
+		if (taxaFreteFinal != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal));
 		}
-		
+
 		criteria.where(predicates.toArray(new Predicate[0]));
 
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 
 	}
-	
-	
 
 }
 
