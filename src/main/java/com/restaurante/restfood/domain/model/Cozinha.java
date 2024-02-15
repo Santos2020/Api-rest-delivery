00dc8e1dcce3dde8f88c149e25b2d11ed86c34ed
carlos santos <carlos.aes.1977@gmail.com>
2024-02-15 16:40:25 -0300

    persistindo dados JPA

--- src/main/java/com/restaurante/restfood/domain/model/Cozinha.java
+++ src/main/java/com/restaurante/restfood/domain/model/Cozinha.java
@@ -1,10 +1,16 @@
 package com.restaurante.restfood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -20,5 +26,9 @@ public class Cozinha {
     
 	@Column(nullable = false)
 	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
--- src/main/java/com/restaurante/restfood/domain/model/Restaurante.java
+++ src/main/java/com/restaurante/restfood/domain/model/Restaurante.java
@@ -1,12 +1,17 @@
+
 package com.restaurante.restfood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,7 +20,7 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Restaurante {
-    
+
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,9 +29,13 @@ public class Restaurante {
 	private String nome;
 
 	private BigDecimal taxaFrete;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
+
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento", joinColumns = @JoinColumn(name = "restaurante_id"), inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
 }
